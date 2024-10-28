42cb1eb4bb53b397713d34a1bbb6e5c5e0f4b6e8
Aavritti and Solvers <aavritti8@gmail.com>
2024-10-28 16:19:28 +0530

    Update unforced_acoustics.py
    corrections and added mean flow solvers and some setup for non linear problems

--- Equations/unforced_acoustics.py
+++ Equations/unforced_acoustics.py
@@ -2,8 +2,6 @@
 # contact : jrvigh@gmail.com
 # -------------------------------------------------------------------------
 
-import os, sys
-file_dir = os.path.dirname(os.path.realpath(__file__))
 '''
 Unforced Acoustics
 ==================
@@ -14,15 +12,19 @@ created by Vighnesh J.R. on 29/7/2024
 
 Contents
 ---------------------
-- `UnforcedAcoustics1` : Uniform base field 
-- `UnforcedAcoustics2` : Non Uniform base field 
-- `UnforcedAcoustics3` : Non Uniform base field with freqency dependent Impedances
+- `UnforcedAcoustics1`   : Uniform base field 
+- `UnforcedAcoustics1_1` : Non Uniform base field with mean flow
+- `UnforcedAcoustics1_2` : Freqency dependent Impedances
 '''
+import os, sys
+file_dir = os.path.dirname(os.path.realpath(__file__))
+
 
 #from equation import Equation
 from dolfin import *
 import numpy as np
 
+###########################################################################################################################
 class UnforcedAcoustics1():
     '''
     Unforced Acoustics ver 1.0
@@ -68,18 +70,18 @@ class UnforcedAcoustics1():
             self.pH_real = Function(geometry.UPn)
             self.pH_imag = Function(geometry.UPn)
         else:
-            self.space = geometry.UP1
+            self.space = geometry.UP2
 
             # Function objects for various solutions
             # Main solution modes
-            self.p_real  = Function(geometry.UP1)
-            self.p_imag  = Function(geometry.UP1)
+            self.p_real  = Function(geometry.UP2)
+            self.p_imag  = Function(geometry.UP2)
 
             # Adjoint Modes
-            self.pH_real = Function(geometry.UP1)
-            self.pH_imag = Function(geometry.UP1) 
+            self.pH_real = Function(geometry.UP2)
+            self.pH_imag = Function(geometry.UP2) 
 
-        pass
+        
 
         self.p_trial = TrialFunction(self.space)
         self.p_test  = TestFunction(self.space)
@@ -123,8 +125,8 @@ class UnforcedAcoustics1():
         print('Building boundary matrices (Damping)')
 
         # Initialisation (Need to figure out if there is a better way without collapse)
-        B_real = 1*inner(self.P_test,self.p_trial) - 1*inner(self.P_test,self.p_trial)
-        B_imag = 1*inner(self.P_test,self.p_trial) - 1*inner(self.P_test,self.p_trial)
+        B_real = 1*inner(self.p_test,self.p_trial)*dx - 1*inner(self.p_test,self.p_trial)*dx
+        B_imag = 1*inner(self.p_test,self.p_trial)*dx - 1*inner(self.p_test,self.p_trial)*dx
 
         if(self.n_refl_boundary==0):
             pass
@@ -136,31 +138,125 @@ class UnforcedAcoustics1():
         self.B_real_weakform = B_real
         self.B_imag_weakform = B_imag
 
-class UnforcedAcoustics2(UnforcedAcoustics1):
+ ###########################################################################################################################
+
+class UnforcedAcoustics1_1(UnforcedAcoustics1):
     '''
-    Unforced Acoustics ver 2.0
+    Unforced Acoustics ver 1.1
+    ============================
+    Created : Vighnesh J.R. on 29/7/2024
+    inherited from version 1.0
+    -----------------------------------
+    New Features
+    --------
+    - Can handle non uniform baseflow that is
+      - Steady
+      - Incompressible mean flow
+      - Low mach number
+    - Cannot handle impedance boundary conditions
+    '''
+    def init_function_spaces(self,geometry,settings):
+
+        # Check if user requires special elements
+        if(settings.is_high_order):
+            self.space = geometry.UPn
+            self.vspace = geometry.VPn
+
+            # Function objects for various solutions
+            # Main solution modes
+            self.p_real  = Function(geometry.UPn) 
+            self.p_imag  = Function(geometry.UPn)
+
+            # Mean flow field
+            self.mean_flow = Function(geometry.VPn)
+
+            # Adjoint Modes
+            self.pH_real = Function(geometry.UPn)
+            self.pH_imag = Function(geometry.UPn)
+        else:
+            self.space = geometry.UP2
+            self.vspace = geometry.VP2
+
+            # Function objects for various solutions
+            # Main solution modes
+            self.p_real  = Function(geometry.UP2)
+            self.p_imag  = Function(geometry.UP2)
+
+            # Mean flow field
+            self.mean_flow = Function(geometry.VP2)
+
+            # Adjoint Modes
+            self.pH_real = Function(geometry.UP2)
+            self.pH_imag = Function(geometry.UP2)
+
+        self.p_trial = TrialFunction(self.space)
+        self.p_test  = TestFunction(self.space)
+
+    def build_Stiffness_matrix(self):
+
+        # Jacobian matrix = ∫(∇v).M ∇.(Mu)dV -∫(∇v).(∇u)dV
+        print('Building stiffness matrix UnforcedAcoustics 1.1')
+        L = inner(grad(self.p_test), self.mean_flow)*div(self.mean_flow*self.p_trial)*dx - inner(grad(self.p_test), grad(self.p_trial))*dx        
+        self.L_weakform = L
+    
+    def build_Damping_matrix(self):
+        
+        # Damping Matrix = 2i∫ v∇.(Mu)dV
+        # Imaginary part omitted
+        print('Bulding damping matrix UnforcedAcoustics 1.1')
+        B = 2*inner(self.p_test,div(self.mean_flow*self.p_trial))*dx
+        self.B_weakform = B
+    
+    def build_Mass_matrix(self):
+
+        # ∫(uv)dV
+        print('Building mass matrix UnforcedAcoustics 1.1')
+        A = inner(self.p_test,self.p_trial)*dx
+        self.A_weakform = A
+
+#########################################################################################################################################
+
+class UnforcedAcoustics1_2(UnforcedAcoustics1):
+    '''
+    Unforced Acoustics ver 1.2
     ============================
     details : To be updated
     inherited from version 1.0 
 
     New Features
     --------
-    - Can handle non uniform baseflow
-    '''
-    pass
-class UnforcedAcoustics3(UnforcedAcoustics2):
-    '''
-    Unforced Acoustics ver 3.0
-    ============================
-    details : To be updated
-    inherited from version 2.0
-
-    New Features
-    --------
     - Can handle frequncy dependent impedances
     - Needs non linear eigenvalue solver
     '''
-    pass
+    def build_Boundary_matrix(self):
+        c0 = Constant(self.constants["c0"])
+
+        # Boundary matrix = 1/(c0) ∮ uv ds_n
+        print('Building boundary matrices (Damping)')
+
+        # Initialisation of list and form of size nxn
+        Bs = []
+        B0 = 1*inner(self.p_test,self.p_trial)*dx - 1*inner(self.p_test,self.p_trial)*dx
+
+        # Appending boundary integrals for each boundary
+        if(self.n_refl_boundary==0):
+            pass
+        else:
+            for n in range(self.n_refl_boundary):
+                Bs.append((1/c0)*inner(self.p_test,self.p_trial)*self.reflective_boundary_list[n]+ B0)
+        
+        # This shall be saved inn result folder as per the needs of the executeable file   
+        self.B_weakform_list = Bs
+
+    # Setting boundary endowed with Frequency dependent Impedance
+    def set_reflective_boundaries(self,
+                                  boundaries =[],    # Pass a list of boundaries ( numbering must match with coefficients)
+                                  ):
+        self.reflective_boundary_list = boundaries
+        self.n_refl_boundary = len(boundaries)
+
+   
+
 
 
 
