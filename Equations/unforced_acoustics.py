2e2f2cea130927a720abf45f86ff0e7feaeb9e4c
Aavritti and Solvers <aavritti8@gmail.com>
2025-04-01 11:41:57 +0530

    modify : unforced acoustics
    added speaker case.. will shift to forced acoustics soon

--- Equations/unforced_acoustics.py
+++ Equations/unforced_acoustics.py
@@ -15,6 +15,7 @@ Contents
 - `UnforcedAcoustics1`   : Uniform base field 
 - `UnforcedAcoustics1_1` : Non Uniform base field with mean flow
 - `UnforcedAcoustics1_2` : Freqency dependent Impedances
+- `SpeakerAcoustics`     : Speaker forced boundary conditions
 '''
 import os, sys
 file_dir = os.path.dirname(os.path.realpath(__file__))
@@ -255,7 +256,84 @@ class UnforcedAcoustics1_2(UnforcedAcoustics1):
         self.reflective_boundary_list = boundaries
         self.n_refl_boundary = len(boundaries)
 
-   
+#########################################################################################################
+class SpeakerAcoustics(UnforcedAcoustics1):
+    def __init__(self,settings,geometry):
+
+        self.constants = settings.constants
+        self.values    = settings.values
+
+        # Initialising named boundaries
+        self.open_boundary              = None
+        self.computed_surface           = None
+        self.speaker_boundary           = None
+
+        # Boundaries where reflectivity is specified
+        self.reflective_boundary_list   = None
+        self.n_refl_boundary            = None
+
+        # Initialise functionspaces
+        self.init_function_spaces(geometry, settings)
+
+    # Setting up speaker
+    def set_speaker_boundary(self,boundaries):
+        self.speaker_boundary = boundaries
+
+    # Open boundary
+    def set_open_boundary(self,boundaries):
+        self.open_boundary = boundaries
+    
+    # Computed surface (for post processing)
+    def set_computed_surface(self,area):
+        self.computed_surface = area
+
+    # Building matrices and tuples
+    def build_Stiffness_matrix(self):
+
+        # Jacobian matrix = -∫(∇v).(∇u)dV
+        print('Building stiffness matrix UnforcedAcoustics1')
+        L = -1*inner(grad(self.p_test),grad(self.p_trial))*dx
+        self.L_weakform = L
+
+    def build_Mass_matrix(self):
+        
+        # Mass matrix  = (1/c0²)∫(uv)dV
+        print('Building Mass matrix UnforcedAcoustics1')
+        A = (1/self.constants["c0"]**2)*inner(self.p_test,self.p_trial)*dx
+        self.A_weakform = A
+
+    def build_Boundary_matrix(self):
+        c0 = Constant(self.constants["c0"])
+
+        # Boundary matrix = i/(Z_n*c0) ∮ uv ds_n
+        print('Building boundary matrices (Damping)')
+
+        # Initialisation (Need to figure out if there is a better way without collapse)
+        B_real = 1*inner(self.p_test,self.p_trial)*dx - 1*inner(self.p_test,self.p_trial)*dx
+        B_imag = 1*inner(self.p_test,self.p_trial)*dx - 1*inner(self.p_test,self.p_trial)*dx
+
+        if(self.n_refl_boundary==0):
+            pass
+        else:
+            for n in range(self.n_refl_boundary):
+                B_real += np.real(1j/c0*self.impedances[n])*inner(self.p_test,self.p_trial)*self.reflective_boundary_list[n]
+                B_imag += np.imag(1j/c0*self.impedances[n])*inner(self.p_test,self.p_trial)*self.reflective_boundary_list[n]
+
+        self.B_real_weakform = B_real
+        self.B_imag_weakform = B_imag
+    
+    def build_Speaker_matrix(self):
+        print('Building Speaker B matrix and forcing tuple')
+        f = self.values["Forcing_magnitude"]
+        null =  1*inner(self.p_test,self.p_trial)*dx - 1*inner(self.p_test,self.p_trial)*dx
+        Bs = null + Constant(1/self.constants["c0"])*inner(self.p_test,self.p_trial)*self.speaker_boundary
+        F  = self.p_test*Constant(f)*self.speaker_boundary 
+
+        self.B_speaker = Bs
+        self.F_speaker = F 
+        
+
+
 
 
 
